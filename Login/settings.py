979b142d4fee1ac5e58487e1b2a5b71c888b98d3
iamajitsingh <iamajit41@gmail.com>
2021-10-12 23:21:46 +0530

    Added profile page, fixed bugs, refactored authentication logic

--- Login/settings.py
+++ Login/settings.py
@@ -11,10 +11,12 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from django.contrib.messages import constants as messages
+
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -27,18 +29,18 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
     'django.contrib.admin',
-    'django.contrib.auth', # This is there by default
+    'django.contrib.auth',  # This is there by default
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'account',
     'passwordGenerator',
+    'userprofile',
 ]
 
 MIDDLEWARE = [
@@ -56,7 +58,7 @@ ROOT_URLCONF = 'Login.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ["templates"], # Added new
+        'DIRS': ["templates"],  # Added new
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -71,7 +73,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'Login.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
@@ -81,7 +82,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -101,20 +101,18 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Calcutta'
 
 USE_I18N = True
 
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
@@ -125,3 +123,10 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+}
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'content')
--- account/views.py
+++ account/views.py
@@ -1,107 +1,67 @@
-from django.shortcuts import render, redirect, HttpResponse
-from django.contrib.auth import authenticate, logout
-from django.contrib.auth import login as dj_login
-from django.contrib.auth.models import User
-from django.contrib import messages
+from django.shortcuts import render, redirect
 from django.views.generic import TemplateView
-from django.contrib.auth import update_session_auth_hash
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout, update_session_auth_hash
+from django.contrib.auth.models import User
 from django.contrib.auth.forms import PasswordChangeForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.db import IntegrityError
+from .forms import CreateUserForm
 
-# Global variable
-authenticated = False
+
 # view for rendering homepage
-
-
 def home(request):
     return render(request, "account/home.html")
 
 
 # view for rendering signup page
-
-
-def signup(request):
-    if authenticated:
-        return render(request, "account/home.html")
-    return render(request, "account/signup.html")
-
-
-# view for rendering login page
-def login(request):
-    if authenticated:
-        return render(request, "account/home.html")
-    return render(request, "account/login.html")
-
-
-# view for rendering data coming from signup page
-
-
-def handlesignup(request):
-    if request.method == 'POST':
-        # get the post parameters
-        uname = request.POST["uname"]
-        fname = request.POST["fname"]
-        lname = request.POST["lname"]
-        email = request.POST["email"]
-        password1 = request.POST["password1"]
-        password2 = request.POST["password2"]
-
-        # check for errors in input
-        if request.method == 'POST':
-            try:
-                user_exists = User.objects.get(username=request.POST['uname'])
-                messages.error(
-                    request, " Username already taken, Try something else!!!")
-                return redirect("/signup")
-            except User.DoesNotExist:
-                if len(uname) > 15:
-                    messages.error(
-                        request, " Username must be max 15 characters, Please try again")
-                    return redirect("/signup")
-
-                if not uname.isalnum():
-                    messages.error(
-                        request, " Username should only contain letters and numbers, Please try again")
-                    return redirect("/signup")
-
-                if password1 != password2:
-                    messages.error(
-                        request, " Password do not match, Please try again")
-                    return redirect("/signup")
-
-        # create the user
-        user = User.objects.create_user(uname, email, password1)
-        user.first_name = fname
-        user.last_name = lname
-        user.save()
-        messages.success(
-            request, " Your account has been successfully created")
-        return redirect("/")
-
+def signupuser(request):
+    if request.user.is_authenticated:
+        return redirect('/')
+    if request.method == 'GET':
+        return render(request, 'account/signup.html', {'form': CreateUserForm()})
     else:
-        return HttpResponse('404 - NOT FOUND ')
+        if len(request.POST['username']) > 15:
+            messages.error(request, " Username must be max 15 characters, Please try again")
+            return render(request, 'account/signup.html', {'form': CreateUserForm()})
+        if not request.POST['username'].isalnum():
+            messages.error(request, "Username should only contain letters and numbers, Please try again")
+            return render(request, 'account/signup.html', {'form': CreateUserForm()})
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.create_user(request.POST['username'], password=request.POST['password1'],
+                                                first_name=request.POST['first_name'],
+                                                last_name=request.POST['last_name'])
+                user.save()
+                login(request, user)
+                return redirect('/')
+            except IntegrityError:
+                messages.error(request, "Username already taken, Please try something else!")
+                return render(request, 'account/signup.html', {'form': CreateUserForm()})
+        else:
+            messages.error(request, "Passwords did not match, Please try something else!")
+            return render(request, 'account/signup.html', {'form': CreateUserForm()})
 
 
-# view for rendering data from login page
-def handlelogin(request):
-    if request.method == 'POST':
-        # get the post parameters
-        name = request.POST["name"]
-        password = request.POST["password"]
-        user = authenticate(username=name, password=password)
+def loginuser(request):
+    if request.user.is_authenticated:
+        return redirect('/')
+    if request.method == 'GET':
+        return render(request, "account/login.html")
+    if request.method == "POST":
+        user_name = request.POST.get('name', '')
+        user_password = request.POST.get('password', '')
 
-        # cheching for valid login
+        user = authenticate(username=user_name, password=user_password)
+
         if user is not None:
-            dj_login(request, user)
+            login(request, user)
             messages.success(request, " Successfully logged in")
-            global authenticated
-            authenticated = True
-            return redirect("/")
+            return redirect('/')
         else:
             messages.error(request, " Invalid Credentials, Please try again")
-            return redirect("/")
-    return HttpResponse('404 - NOT FOUND ')
+            return render(request, 'account/home.html')
 
 
 # view for rendering logout
@@ -109,8 +69,6 @@ def handlelogin(request):
 def handlelogout(request):
     logout(request)
     messages.success(request, " Successfully logged out")
-    global authenticated
-    authenticated = False
     return redirect('/')
 
 
